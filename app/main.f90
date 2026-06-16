7b9733955518ad0bf2fd9c52b7aeb811841b4432
pepe_icn <antonio.garcia.lattice@gmail.com>
2026-06-16 17:52:20 -0600

    newer version

--- app/main.f90
+++ app/main.f90
@@ -8,22 +8,25 @@ program main
   integer, parameter :: Lx = 16
   integer, parameter :: Lt = 16
   integer, parameter :: Nt = 100
+  integer, parameter :: top_sector = 0
 
   real(dp), dimension(Lx,Lt) :: Q
 
-  integer, parameter :: nconfig = 10000
+  integer, parameter :: nconfig = 1000
   real(dp), parameter :: pi = acos(-1.0_dp)
 
   real(dp) :: beta = 3.0_dp
   real(dp) :: m0 = 100.0_dp
   real(dp) :: epsilon = 1.0E-2_dp
   
-  complex(dp), dimension(2,Lx,Lt) :: U
+  complex(dp), dimension(2,Lx,Lt) :: U, V
   character(:), allocatable :: filename
 
   integer :: i, un, count, ix, it, t
 
   real(dp) :: a_action(nconfig), slb(nconfig,Lx,0:Nt), E(nconfig,0:Nt)
+
+  INTEGER :: Q0, Qf
   
 
   
@@ -31,9 +34,9 @@ program main
 
   
   
-  open(newunit = un, file = "energy.dat")
-  open(unit = 69, file = "q1.dat")
-  open(unit = 42, file = "slab.dat")
+  open(newunit = un, file = "energy_Q0.dat")
+  open(unit = 69, file = "q0.dat")
+  open(unit = 42, file = "slab_Q0.dat")
   count = 0
   slb = 0.0_dp
   do i = 1, nconfig
@@ -43,12 +46,22 @@ program main
           "/U_"//int2str(i)//".bin"
      call read_configuration(U,filename)
 
-     
-     if( nint(abs(top_char(U))) == 1 ) then
-        count = count + 1        
-        call wilson_flow(U,beta,Nt,epsilon)
-     end if
+     V = U
+     Q0 = nint(abs(top_char(U)))
+     if(Q0 == top_Sector)then
+        !count = count + 1
+        call wilson_flow_rk4(V,beta,Nt,epsilon,.false.)
+        Qf = nint(abs(top_char(V)))
+        
+        if( Q0 == Qf ) then
+           count = count + 1
+           Print*, count, i, Q0, Qf, Q0==Qf
+           call wilson_flow_rk4(U,beta,Nt,epsilon,.true.)
+        end if
+     END if
+        
 
+        
   end do
 
 
@@ -228,7 +241,7 @@ contains
     do i = 1, Lx
        do j = 1, Ly
           plq = plaquette(u,[i,j])
-          top_char = top_char + atan2(plq%im,plq%re)!/(2*pi)
+          top_char = top_char + atan2(plq%im,plq%re)
        end do
     end do
     top_char = top_char/(2*pi)
@@ -307,12 +320,13 @@ contains
 
 
 
-  subroutine wilson_flow_rk4(U,beta,Nt,epsilon) !result(Up)
+  subroutine wilson_flow_rk4(U,beta,Nt,epsilon,condition) !result(Up)
     complex(dp), intent(inout) :: U(:,:,:)
     complex(dp), dimension(2,size(U(1,:,1)),size(U(1,1,:))) :: W0, W1, W2, W3, Z0, Z1, Z2, B
     real(dp), intent(in) :: beta
     real(dp), intent(in) :: epsilon
-    integer, intent(in) :: Nt 
+    integer, intent(in) :: Nt
+    logical,intent(in) :: condition
     integer :: x,y,t, Lx, Ly
     
     !real(dp) :: slb(size(u(1,:,1)))
@@ -320,13 +334,16 @@ contains
     Lx = size(U(1,:,1))
     Ly = size(U(1,1,:))
 
-    Q = top_char_arr(U)
-    E(count,0) = energy(U)
     
-    write(un,*) 0.0_dp, energy(U), sum(top_char_arr(U))/(2*pi)
-    do ix = 1, Lx
-       slb(count,ix,0) = slab_top_char(Q,ix)
-    end do
+    if(condition) then
+       Q = top_char_arr(U)
+       E(count,0) = energy(U)
+       write(un,*) 0.0_dp, energy(U), sum(Q)/(2*pi)
+       do ix = 1, Lx
+          slb(count,ix,0) = slab_top_char(Q,ix)
+          write(69,*) 0.0_dp, i, slb(count,ix,0)
+       end do
+    end if
     do t = 1, Nt
 
        W0 = U
@@ -360,15 +377,21 @@ contains
        W3 = exp(0.75_dp*Z2)*W1
        
        U = W3
-       Q = top_char_arr(U)
        
-       E(count,t) = energy(U)
-       write(un,*) t*epsilon, energy(U), sum(top_char_arr(U))/(2*pi)
-       do ix = 1, Lx
-          slb(count,ix,t) = slab_top_char(Q,ix)
-       end do
+       if(condition) then
+          Q = top_char_arr(U)
+          E(count,t) = energy(U)
+          write(un,*) t*epsilon, E(count,t), sum(Q)/(2*pi)
+          !write(*,*) t*epsilon, E(count,t), sum(Q)/(2*pi)
+          do ix = 1, Lx
+             slb(count,ix,t) = slab_top_char(Q,ix)
+             write(69,*) i, slb(count,ix,t)
+          end do
+       end if
+       
+       
     end do
-    write(un,"(2/)") 
+    if(condition) write(un,"(2/)") 
 
   end subroutine wilson_flow_rk4
 
