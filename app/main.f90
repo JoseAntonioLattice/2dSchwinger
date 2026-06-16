8a67511ddcb2eafccdf800f7f297945e6d465a42
pepe <antonio.garcia.lattice@gmail.com>
2026-06-15 23:33:27 -0600

    newer data

--- app/main.f90
+++ app/main.f90
@@ -7,11 +7,11 @@ program main
 
   integer, parameter :: Lx = 16
   integer, parameter :: Lt = 16
-  integer, parameter :: Nt = 50
+  integer, parameter :: Nt = 100
 
   real(dp), dimension(Lx,Lt) :: Q
 
-  integer, parameter :: nconfig = 1000
+  integer, parameter :: nconfig = 10000
   real(dp), parameter :: pi = acos(-1.0_dp)
 
   real(dp) :: beta = 3.0_dp
@@ -23,12 +23,14 @@ program main
 
   integer :: i, un, count, ix, it, t
 
-  real(dp) :: a_action(nconfig), slb(Lx,0:Nt)
-  real(dp), dimension(nconfig,Lx,Nt) :: slab
+  real(dp) :: a_action(nconfig), slb(nconfig,Lx,0:Nt), E(nconfig,0:Nt)
+  
 
   
   call set_pbc([Lx,Lt])
 
+  
+  
   open(newunit = un, file = "energy.dat")
   open(unit = 69, file = "q1.dat")
   open(unit = 42, file = "slab.dat")
@@ -41,24 +43,51 @@ program main
           "/U_"//int2str(i)//".bin"
      call read_configuration(U,filename)
 
-     !write(un,*) t*epsilon, action(U), top_char(Up)
+     
      if( nint(abs(top_char(U))) == 1 ) then
-        count = count + 1
+        count = count + 1        
         call wilson_flow(U,beta,Nt,epsilon)
      end if
-     !write(un,*) t*epsilon, action(U), top_char(U)
+
   end do
 
+
+  
   do t = 0, Nt
      do ix = 1, Lx
-        write(42,*) 1.0_dp*ix/(Lx), slb(ix,t)/count
+        write(42,*) 1.0_dp*ix/(Lx), avr(slb(1:count,ix,t)), std_err(slb(1:count,ix,t))
      end do
      write(42,"(2/)")
+     print*, t*epsilon, avr(E(1:count,t)),std_err(E(1:count,t))
   end do
   print*, count
   
 contains
 
+  function avr(x)
+    real(dp), intent(in) :: x(:)
+    real(dp) :: avr
+
+    avr = sum(x)/size(x)
+    
+  end function avr
+
+  function var(x)
+    real(dp), intent(in) :: x(:)
+    real(dp) :: var
+
+    var = sum((x - avr(x))**2)/(size(x)-1) 
+    
+  end function var
+
+  function std_err(x)
+    real(dp), intent(in) :: x(:)
+    real(dp) :: std_err
+
+    std_err = sqrt(var(x)/size(x))
+    
+  end function std_err
+  
   subroutine read_configuration(U,filename)
     use number2string
     complex(dp), intent(out) :: U(:,:,:)
@@ -126,6 +155,26 @@ contains
     end do
 
   end function action
+
+    function energy(u)
+    real(dp) :: energy
+    complex(dp), dimension(:,:,:), intent(in) :: u
+    integer(i4) :: x,y, Lx, Ly
+
+    Lx = size(u(1,:,1))
+    Ly = size(u(1,1,:))
+    
+    energy = 0.0_dp
+    
+    do x = 1, size(u(1,:,1))
+       do y = 1,  size(u(1,1,:))
+          energy = energy + real(plaquette(u,[x,y]))
+       end do
+    end do
+
+    energy = Lx*Ly-energy
+    
+  end function energy
   
   function correlation_polyakov(U) result(corr_poly)
     complex(dp), intent(in) :: U(:,:,:)
@@ -197,7 +246,7 @@ contains
     do i = 1, Lx
        do j = 1, Ly
           plq = plaquette(u,[i,j])
-          top_char_arr(i,j) =  plq%im!atan2(plq%im,plq%re)!/(2*pi)
+          top_char_arr(i,j) =  atan2(plq%im,plq%re)!/(2*pi)
        end do
     end do
     !top_char = top_char!/(2*pi)
@@ -230,8 +279,11 @@ contains
     Ly = size(U(1,1,:))
 
     Q = top_char_arr(U)
+    E(count,0) = energy(U)
+    
+    write(un,*) 0.0_dp, energy(U), sum(top_char_arr(U))/(2*pi)
     do ix = 1, Lx
-       slb(ix,0) = slb(ix,0) + slab_top_char(Q,ix)
+       slb(count,ix,0) = slab_top_char(Q,ix)
     end do
     do t = 1, Nt
        do x = 1, Lx
@@ -242,12 +294,90 @@ contains
        end do
        U = Up
        Q = top_char_arr(U)
-        do ix = 1, Lx
-           slb(ix,t) = slb(ix,t) + slab_top_char(Q,ix)
-        end do
+       
+       E(count,t) = energy(U)
+       write(un,*) t*epsilon, energy(U), sum(top_char_arr(U))/(2*pi)
+       do ix = 1, Lx
+          slb(count,ix,t) = slab_top_char(Q,ix)
+       end do
     end do
     write(un,"(2/)") 
 
   end subroutine wilson_flow
+
+
+
+  subroutine wilson_flow_rk4(U,beta,Nt,epsilon) !result(Up)
+    complex(dp), intent(inout) :: U(:,:,:)
+    complex(dp), dimension(2,size(U(1,:,1)),size(U(1,1,:))) :: W0, W1, W2, W3, Z0, Z1, Z2, B
+    real(dp), intent(in) :: beta
+    real(dp), intent(in) :: epsilon
+    integer, intent(in) :: Nt 
+    integer :: x,y,t, Lx, Ly
+    
+    !real(dp) :: slb(size(u(1,:,1)))
+
+    Lx = size(U(1,:,1))
+    Ly = size(U(1,1,:))
+
+    Q = top_char_arr(U)
+    E(count,0) = energy(U)
+    
+    write(un,*) 0.0_dp, energy(U), sum(top_char_arr(U))/(2*pi)
+    do ix = 1, Lx
+       slb(count,ix,0) = slab_top_char(Q,ix)
+    end do
+    do t = 1, Nt
+
+       W0 = U
+       do x = 1, Lx
+          do y = 1, Ly
+             Z0(1,x,y) = epsilon*Zeta(W0,[x,y],1,beta)
+             Z0(2,x,y) = epsilon*Zeta(W0,[x,y],2,beta)
+             
+          end do
+       end do
+
+       W1 = exp(0.25_dp*Z0)*W0
+
+       do x = 1, Lx
+          do y = 1, Ly
+             Z1(1,x,y) = epsilon*Zeta(W1,[x,y],1,beta)
+             Z1(2,x,y) = epsilon*Zeta(W1,[x,y],2,beta)
+          end do
+       end do
+
+       B = 8.0_dp/9.0_dp*Z1-17.0_dp/36.0_dp*Z0
+       W2 = exp(B)*W1
+
+       do x = 1, Lx
+          do y = 1, Ly
+             Z2(1,x,y) = epsilon*Zeta(W2,[x,y],1,beta)
+             Z2(2,x,y) = epsilon*Zeta(W2,[x,y],2,beta)
+          end do
+       end do
+
+       W3 = exp(0.75_dp*Z2)*W1
+       
+       U = W3
+       Q = top_char_arr(U)
+       
+       E(count,t) = energy(U)
+       write(un,*) t*epsilon, energy(U), sum(top_char_arr(U))/(2*pi)
+       do ix = 1, Lx
+          slb(count,ix,t) = slab_top_char(Q,ix)
+       end do
+    end do
+    write(un,"(2/)") 
+
+  end subroutine wilson_flow_rk4
+
+  function Zeta(U,x,mu,beta)
+    complex(dp) :: Zeta
+    complex(dp), intent(in) :: U(:,:,:)
+    integer, intent(in) :: x(2), mu
+    real(dp) :: beta
+    Zeta = cmplx( 0.0_dp, -beta*aimag(U(mu,x(1),x(2))*conjg(staples(U,x,mu))), dp)
+  end function Zeta
 
 end program main
